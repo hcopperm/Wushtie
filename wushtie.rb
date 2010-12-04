d1c5e74f36b681de3d0ff384a03a33da639dd58e
hcopperm <hcopperm@gmail.com>
2010-12-03 17:44:09 -0800

    added user input

--- wushtie.rb
+++ wushtie.rb
@@ -1,56 +1,66 @@
 #!usr/local/bin/ruby -w
-module Bing
-	class Wushtie
+module Sim
+	class Wush
+
 		def initialize
-			@name = "Yonder Wudge"
-			@fuds = [] 
-			@litterbox = []
+			@lexicon = {
+				"i" => "***mew mew mew***",
+				"f" => "***nom***",
+				"p" => "***purr***"	
+			}
 		end
 
-		def say_mew
-			puts "mew mew mew"
-		end
-		
-		def foods_time?
-			if near_enough?(Time.now.hour, 17, 7)
-				true
+		def start
+			if foods_time?
+				foods_time_prompts
+			else
+				puts "The kitty isn't hungry."	
 			end
 		end
 
-		def alert
-			if foods_time? and @fuds.empty?
-				say_mew
-			elsif !@litterbox.empty? 
-				say_mew
-			end 
+		
+
+		def foods_time_prompts
+			puts "Wushtie wants to be fed. Would you like to feed her? Press 'i' to ignore her, 'f' to feed her, or 'p' to pet her."			
+				s = usr_input
+				if @lexicon[s] != nil
+					puts @lexicon[s]
+					if s == "i"
+						puts "Wushtie is not happy about being ignored."
+						start
+					elsif s == "p"
+						puts "Wushtie likes the petting but she is still hungry."
+						start
+					end
+				else
+					puts "That doesn't mean anything. The wushtie is still hungry!"
+					start	
+				end	
+			
+
+
 		end
 
-		#are we within 3 hours of a time?
+### Utils ###
+
+#		#are we within 3 hours of a time?
 		def near_enough?(time1, time2, time3)
 			if (time1-time2).abs <= 3 || (time1-time3).abs <= 3
 				true
  			end
 		end
 
-		def poop
-			@litterbox << "poop" 
+		def foods_time?
+			if near_enough?(Time.now.hour, 17, 7)
+				true
+			end
 		end
 
+		def usr_input
+			STDIN.readline.strip!
+		end
+	
 	end
-
-	class Person
-		def initialize
-			@name="d00d"
-		end
-		
-		def feed_wushtie(wushtie, foods)
-			wushtie.fuds << foods
-		end
-
-	end
-
 end
 
-wushtie = Bing::Wushtie.new
 
-wushtie.alert
