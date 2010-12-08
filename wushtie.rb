0b0736061d090a29c85bbfa2aceaeeaff8b3a365
hcopperm <hcopperm@gmail.com>
2010-12-08 15:59:39 -0800

    got rid of my awesome recursivetimez :-(

--- wushtie.rb
+++ wushtie.rb
@@ -1,45 +1,44 @@
 #!usr/local/bin/ruby -w
 module Sim
-	class Wush
+	class Game
 
 		def initialize
+			@points = 0
 			@lexicon = {
-				"i" => "***mew mew mew***",
-				"f" => "***nom***",
-				"p" => "***purr***"	
+				"i" => UserCommand.new("you fed the cat") {},
+				"p" => UserCommand.new("she likes petting but still wants food") {@petting_times += 1; get_angry},
+				"f" => UserCommand.new("Good job, you have successfully fed the cat") {@points += 1}, 
+				"s" => UserCommand.new("You have this many points") {puts @points},
+				"q" => UserCommand.new("bye!") {throw :break_out}
 			}
+			@petting_times = 0
+
 		end
 
 		def start
-			if foods_time?
-				foods_time_prompts
-			else
-				puts "The kitty isn't hungry."	
-			end
-		end
-
-		
-
-		def foods_time_prompts
-			puts "Wushtie wants to be fed. Would you like to feed her? Press 'i' to ignore her, 'f' to feed her, or 'p' to pet her."			
-				s = usr_input
-				if @lexicon[s] != nil
-					puts @lexicon[s]
-					if s == "i"
-						puts "Wushtie is not happy about being ignored."
-						start
-					elsif s == "p"
-						puts "Wushtie likes the petting but she is still hungry."
-						start
+			puts "There is a wushtie."
+			catch :break_out do
+				loop{
+					reply = usr_input 
+					if !@lexicon[reply].nil?
+						@lexicon[reply].execute 
+					else
+						puts "I'm sorry, I don't know what that means."
 					end
-				else
-					puts "That doesn't mean anything. The wushtie is still hungry!"
-					start	
-				end	
-			
-
+				} 
+			end
 
 		end
+		def get_angry
+			if @petting_times == 2
+				puts "wushtie is getting tired of the petting"
+			elsif @petting_times >= 3
+				puts "wushtie has had enough of the petting. you lose a point"
+				@points -= 1 
+			end	
+		 
+		end
+
 
 ### Utils ###
 
@@ -59,8 +58,26 @@ module Sim
 		def usr_input
 			STDIN.readline.strip!
 		end
-	
+
 	end
+
+	#compy's response + appropriate code to execute	
+	class UserCommand	
+		def initialize(response, &block)
+			@response = response
+			@action = block
+		end
+
+		def execute
+			puts @response
+			@action.call
+		end
+
+
+	end
+
+
 end
 
+Sim::Game.new.start
 
