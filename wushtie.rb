1390f0edd2d5e43944fa98baa0c66472e294d60e
hcopperm <hcopperm@gmail.com>
2010-12-21 11:17:06 -0800

    added new changes

--- wushtie.rb
+++ wushtie.rb
@@ -5,18 +5,27 @@ module Sim
 		def initialize
 			@points = 0
 			@lexicon = {
-				"i" => UserCommand.new("you fed the cat") {},
-				"p" => UserCommand.new("she likes petting but still wants food") {@petting_times += 1; get_angry},
-				"f" => UserCommand.new("Good job, you have successfully fed the cat") {@points += 1}, 
-				"s" => UserCommand.new("You have this many points") {puts @points},
+				"i" => UserCommand.new("You ignored the wushtie.") {@not_feeding_times += 1; @make_her_mad[@not_feeding_times].execute},
+				"p" => UserCommand.new("You pet the wushtie.") {@not_feeding_times += 1; @make_her_mad[@not_feeding_times].execute},
+				"f" => UserCommand.new("Good job, you have successfully fed the cat.") {@points += 1}, 
+				"s" => UserCommand.new("You have this many points:") {puts @points},
+				"r" => UserCommand.new("There is a wushtie. You can [f]eed her, [p]et her, [i]gnore her, or press [s] to see how many points you have. After you've fed her, you can [q]uit. Press [r] to see this message again.") {},
 				"q" => UserCommand.new("bye!") {throw :break_out}
 			}
-			@petting_times = 0
+
+			@make_her_mad = {
+				1 => UserCommand.new("But she is still hungry.") {},
+				2 => UserCommand.new("Wushtie is getting tired of that.") {},
+				3 => UserCommand.new("Wushtie has had enough of that. You lose a point") {@points -= 1},
+				4 => UserCommand.new("Too much of that, not enough feeding. You lose. Goodbye!") { throw :break_out}
+			}
+
+			@not_feeding_times = 0
 
 		end
 
 		def start
-			puts "There is a wushtie."
+			puts @lexicon["r"].execute 
 			catch :break_out do
 				loop{
 					reply = usr_input 
@@ -29,16 +38,6 @@ module Sim
 			end
 
 		end
-		def get_angry
-			if @petting_times == 2
-				puts "wushtie is getting tired of the petting"
-			elsif @petting_times >= 3
-				puts "wushtie has had enough of the petting. you lose a point"
-				@points -= 1 
-			end	
-		 
-		end
-
 
 ### Utils ###
 
