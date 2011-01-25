e77ebd98df9299cc3f34567328c0ecdbff6dc274
hcopperm <hcopperm@gmail.com>
2011-01-25 12:18:03 -0800

    added some new parsey type stuff

--- wushtie.rb
+++ wushtie.rb
@@ -2,26 +2,31 @@
 module Sim
 	class Game
 
+		ACTIONWORDS = ["feed", "pet", "ignore", "brush"]
+
+		THINGS = ["wushtie"]
+
+
 		def initialize
 			@points = 0
 			@lexicon = {
 				"i" => UserCommand.new("You ignored the wushtie.") {@not_feeding_times += 1; @make_her_mad[@not_feeding_times].execute},
 				"p" => UserCommand.new("You pet the wushtie.") {@not_feeding_times += 1; @make_her_mad[@not_feeding_times].execute},
-				"f" => UserCommand.new("Good job, you have successfully fed the cat.") {@points += 1}, 
-				"s" => UserCommand.new("You have this many points:") {puts @points},
-				"r" => UserCommand.new("There is a wushtie. You can [f]eed her, [p]et her, [i]gnore her, or press [s] to see how many points you have. After you've fed her, you can [q]uit. Press [r] to see this message again.") {},
+				"feed" => UserCommand.new("Good job, you have successfully fed the cat.") {@not_feeding_times -= 1;if @not_feeding_times < 0; @not_feeding_times = 0;end}, 
+				"s" => UserCommand.new("You are bleeding this much:") {puts @bleeding},
+				"r" => UserCommand.new("There is a wushtie. You can feed her, [p]et her, [i]gnore her, or press [s] to see how much you are bleeding. After you've fed her, you can [q]uit. Press [r] to see this message again.") {},
 				"q" => UserCommand.new("bye!") {throw :break_out}
 			}
 
 			@make_her_mad = {
 				1 => UserCommand.new("But she is still hungry.") {},
 				2 => UserCommand.new("Wushtie is getting tired of that.") {},
-				3 => UserCommand.new("Wushtie has had enough of that. You lose a point") {@points -= 1},
-				4 => UserCommand.new("Too much of that, not enough feeding. You lose. Goodbye!") { throw :break_out}
+				3 => UserCommand.new("Wushtie has had enough of this no foods business. You get a warning pat.") {},
+				4 => UserCommand.new("Not enough feeding. You get whacked.") {@bleeding += 1;@not_feeding_times = 0}
 			}
 
 			@not_feeding_times = 0
-
+			@bleeding = 0
 		end
 
 		def start
@@ -37,7 +42,16 @@ module Sim
 				} 
 			end
 
+			
 		end
+		def parse_user_input(input)
+				parts = input.split(" ")
+				
+				if ACTIONWORDS.include?(parts[0]) 
+					puts "hallo"
+				end
+				
+			end
 
 ### Utils ###
 
@@ -78,5 +92,5 @@ module Sim
 
 end
 
-Sim::Game.new.start
+#Sim::Game.new.start
 
